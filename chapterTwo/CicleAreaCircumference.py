498c958527007386e0d7c0803890fab5cf570343
Omolara Nwaezeigwe-eze <davinel.davinel@gmail.com>
2021-04-29 01:55:05 +0100

    exercise

---
+++ chapterTwo/CicleAreaCircumference.py
@@ -0,0 +1,20 @@
+radius_r = 2
+diameter =2 * radius_r
+circumference = 2 * (3.14159 * radius_r)
+area = 3.14159 * (radius_r * radius_r)
+
+print(diameter, circumference, area)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
