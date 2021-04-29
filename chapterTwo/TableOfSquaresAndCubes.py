6031ac79e2e14a7ff392a83a9de0e6a6c44ca19b
Omolara Nwaezeigwe-eze <davinel.davinel@gmail.com>
2021-04-29 20:20:54 +0100

    exercise

---
+++ chapterTwo/TableOfSquaresAndCubes.py
@@ -0,0 +1,28 @@
+
+
+print("number",'\t', "square",'\t', "cube")
+number = 0
+square = int(number ** 2)
+cube = number * number * number
+print(number,'\t','\t',square,'\t','\t','\t',cube)
+number = 1
+square = int(number ** 2)
+cube = number * number * number
+print(number,'\t','\t',square,'\t','\t','\t',cube)
+number = 2
+square = int(number ** 2)
+cube = number * number * number
+print(number,'\t','\t',square,'\t','\t','\t',cube)
+number = 3
+square = int(number ** 2)
+cube = number * number * number
+print(number,'\t','\t',square,'\t','\t','\t',cube)
+number = 4
+square = int(number ** 2)
+cube = number * number * number
+print(number,'\t','\t',square,'\t','\t',cube)
+number = 5
+square = int(number ** 2)
+cube = number * number * number
+print(number,'\t','\t',square,'\t','\t',cube)
+
