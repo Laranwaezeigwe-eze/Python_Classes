a30b97aefba36a748cf3eac0b68e48c4ed7c5118
Omolara Nwaezeigwe-eze <davinel.davinel@gmail.com>
2021-08-04 01:52:20 +0100

    exercise

--- chapterThree/validating_user_input.py
+++ chapterThree/validating_user_input.py
@@ -1,12 +1,15 @@
 passes = 0
 failures = 0
-counter = 0
 
-grade = int(input("Enter result (1=pass, 2=fail):"))
-while
-if grade == 1:
+total_count = 0
+while total_count < 10:
+    result = int(input('Enter result (1=pass, 2=fail): '))
+    if result == 1:
         passes = passes + 1
+    if result == 2:
+        failures = failures + 1
+    total_count = passes + failures
+print("Passed: ", passes)
+print("Failed: ", failures)
+print(total_count)
 
-        print('Passed:', passes)
-
-        print('Failed:', failures)
