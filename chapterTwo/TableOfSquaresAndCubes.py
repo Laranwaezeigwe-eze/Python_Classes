0fff2e537ab8ac655b377cfe7a7eec366c349267
Omolara Nwaezeigwe-eze <davinel.davinel@gmail.com>
2021-08-04 02:04:37 +0100

    exercise

--- chapterTwo/TableOfSquaresAndCubes.py
+++ chapterTwo/TableOfSquaresAndCubes.py
@@ -1,28 +1,25 @@
-
-
-print("number",'\t', "square",'\t', "cube")
+print("number", '\t', "square", '\t', "cube")
 number = 0
 square = int(number ** 2)
-cube = number * number * number
-print(number,'\t','\t',square,'\t','\t','\t',cube)
+cube = number ** 3
+print(number, '\t', '\t', square, '\t', '\t', '\t', cube)
 number = 1
 square = int(number ** 2)
-cube = number * number * number
-print(number,'\t','\t',square,'\t','\t','\t',cube)
+cube = number ** 3
+print(number, '\t', '\t', square, '\t', '\t', '\t', cube)
 number = 2
 square = int(number ** 2)
-cube = number * number * number
-print(number,'\t','\t',square,'\t','\t','\t',cube)
+cube = number ** 3
+print(number, '\t', '\t', square, '\t', '\t', '\t', cube)
 number = 3
 square = int(number ** 2)
-cube = number * number * number
-print(number,'\t','\t',square,'\t','\t','\t',cube)
+cube = number ** 3
+print(number, '\t', '\t', square, '\t', '\t', '\t', cube)
 number = 4
 square = int(number ** 2)
-cube = number * number * number
-print(number,'\t','\t',square,'\t','\t',cube)
+cube = number ** 3
+print(number, '\t', '\t', square, '\t', '\t', cube)
 number = 5
 square = int(number ** 2)
-cube = number * number * number
-print(number,'\t','\t',square,'\t','\t',cube)
-
+cube = number ** 3
+print(number, '\t', '\t', square, '\t', '\t', cube)
---
+++ self_practice/py/__init__.py
@@ -0,0 +1,61 @@
+# is_old = False
+#
+# is_licensed = False
+#
+# if is_old:
+#     print("You are old enought to drive!")
+# elif is_licensed:
+#     print("You can drive now!")
+# else:
+#     print("You are not old enough!")
+
+# is_magician = True
+# is_expert = True
+#
+# if is_magician and is_expert:
+#     print("You are a master magician")
+# if is_magician and not is_expert:
+#     print("at least you're getting there")
+# if not is_magician:
+#     print("You need magic powers")
+
+# for item in [1,2,3,4]:
+#     for next_item in ['tin','can','milk','juice']:
+#         print(item, next_item)
+
+# counter = 0
+# my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for number in my_list:
+#     counter = counter + number
+# print(counter   )
+#
+# import random
+# frequency1 =0
+# frequency2 =0
+# frequency3 = 0
+# frequency4 = 0
+# frequency5 = 0
+# frequency6 = 0
+# for roll in range(6000000):
+#     face = random.randrange(1,7)
+#     if face == 1:
+#         frequency1 += 1
+#     elif face == 2:
+#         frequency2 += 1
+#     elif face == 3:
+#         frequency3 +=1
+#     elif face == 4:
+#         frequency4 += 1
+#     elif face == 5:
+#         frequency5 += 1
+#     elif face == 6:
+#         frequency6 += 1
+# print(f'Face{"Frequency":>10}')
+# print(f'{1:>2}{frequency1:>10}')
+# print(f'{2:>2}{frequency2:>10}')
+# print(f'{3:>2}{frequency3:>10}')
+# print(f'{4:>2}{frequency4:>10}')
+# print(f'{5:>2}{frequency5:>10}')
+# print(f'{6:>2}{frequency6:>10}')
+
+
