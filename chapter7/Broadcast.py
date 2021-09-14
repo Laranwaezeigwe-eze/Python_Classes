356adf7b85a33f5ce078d617cb9e7ad57f63699a
Omolara Nwaezeigwe-eze <davinel.davinel@gmail.com>
2021-09-14 05:17:01 +0100

    chapter7 exercise

---
+++ chapter7/Broadcast.py
@@ -0,0 +1,7 @@
+import numpy as np
+numbers = np.arange(0,3)
+print(numbers)
+print("cube of number is ",numbers ** 3)
+print("Addition of numbers is: ", numbers + 7)
+print("multiply numbers ", numbers * 2)
+
