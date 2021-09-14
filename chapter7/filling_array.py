9ee99f17d08b091d4f763a04c5336122c4a9ac9a
Omolara Nwaezeigwe-eze <davinel.davinel@gmail.com>
2021-09-14 05:19:13 +0100

    array

---
+++ chapter7/filling_array.py
@@ -0,0 +1,9 @@
+import numpy as np
+array= np.ones((2,3), dtype=int)
+print(array)
+print('*******************')
+array2 = np. zeros((3,3),dtype =int)
+print(array2)
+print("****************************")
+full_array = np.full((2,5),7)
+print(full_array)
